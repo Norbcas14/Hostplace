51fa539356affe53c6f1faf344a1b83b5281ae31
Juan Arango <juanmaracho10@gmail.com>
2022-10-08 03:36:41 -0500

    Solucionando problemas con modelos y botones || ERROR en helper 'cl_image_tag'

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
+  belongs_to :user
   has_one_attached :photo
   has_many :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :bookings
+  has_many :places
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
